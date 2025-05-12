43e5ed2340e6618ddf9266c7fa2508f14c46a41a
Leonardo Rodrigues Gomes <leonardo.r.gomes@Hotmail.com>
2025-05-12 09:23:24 -0300

    test: add unit test for RegisterUserValidator using Shouldly and Bogus builder

---
+++ Tomou.TestUtils/Users/Register/Requests/RequestRegisterUserJsonBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Tomou.Communication.Requests.User;
+using Bogus;
+
+namespace Tomou.TestUtils.Users.Register.Requests;
+public static class RequestRegisterUserJsonBuilder
+{
+    public static RequestRegisterUserJson Build()
+    {
+        return new Faker<RequestRegisterUserJson>()
+            .RuleFor(user => user.Name, faker => faker.Person.FirstName)
+            .RuleFor(user => user.Email, (faker, user) => faker.Internet.Email(user.Name))
+            .RuleFor(user => user.Password, faker => faker.Internet.Password(prefix: "!Aa1"));
+    }
+}
---
+++ Tomou.UnitTests/Users/Register/RegisterUserValidatorTest.cs
@@ -0,0 +1,18 @@
+﻿using Shouldly;
+using Tomou.Application.UseCases.User.Register;
+using Tomou.TestUtils.Users.Register.Requests;
+
+namespace Tomou.UnitTests.Users.Register;
+public class RegisterUserValidatorTest
+{
+    [Fact]
+    public void Success()
+    {
+        var validator = new RegisterUserValidator();
+        var request = RequestRegisterUserJsonBuilder.Build();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeTrue();
+    }
+}
