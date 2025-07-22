6095ac65ea7d02ec35c118b9fd544ec56cf009ee
Leonardo Rodrigues Gomes <leonardo.r.gomes@Hotmail.com>
2025-07-22 08:47:49 -0300

    feat: ResponseMedicationShortJson now returns all of values of medication

--- Tomou.Communication/Responses/Medications/Get/ResponseMedicationShortJson.cs
+++ Tomou.Communication/Responses/Medications/Get/ResponseMedicationShortJson.cs
@@ -1,7 +1,13 @@
 ﻿namespace Tomou.Communication.Responses.Medications.Get;
+
 public class ResponseMedicationShortJson
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Dosage { get; set; } = string.Empty;
+    public DateOnly StartDate { get; set; }
+    public DateOnly EndDate { get; set; }
+    public List<string> TimesToTake { get; set; } = new();
+    public List<string> DaysOfWeek { get; set; } = new();
+
 }
