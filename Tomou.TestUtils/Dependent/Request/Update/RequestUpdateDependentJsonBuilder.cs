9c4ea05889284bc3b517484c7ce90b76f51caf38
Leonardo Rodrigues Gomes <leonardo.r.gomes@Hotmail.com>
2025-07-23 15:15:10 -0300

    test(validator): update dependent

---
+++ Tomou.TestUtils/Dependent/Request/Update/RequestUpdateDependentJsonBuilder.cs
@@ -0,0 +1,12 @@
+﻿namespace Tomou.TestUtils.Dependent.Request.Update;
+using Bogus;
+using Tomou.Communication.Requests.Dependent.Register;
+
+public static class RequestUpdateDependentJsonBuilder
+{
+    public static RequestUpdateDependentJson Build()
+    {
+        return new Faker<RequestUpdateDependentJson>()
+            .RuleFor(dependent => dependent.Name, faker => faker.Person.FirstName);
+    }
+}
---
+++ Tomou.UnitTests/Validators/Dependent/Update/UpdateDependentValidatorTest.cs
@@ -0,0 +1,38 @@
+﻿using Tomou.Application.UseCases.Dependent.Register;
+using Shouldly;
+using Tomou.TestUtils.Dependent.Request;
+using Tomou.Application.UseCases.Dependent.Update;
+using Tomou.TestUtils.Dependent.Request.Update;
+using Tomou.Exception;
+
+namespace Tomou.UnitTests.Validators.Dependent.Update;
+public class UpdateDependentValidatorTest
+{
+    [Fact]
+
+    public void Success()
+    {
+        var validator = new UpdateDependentValidator();
+        var request = RequestUpdateDependentJsonBuilder.Build();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeTrue();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("         ")]
+    public void Error_Name_Empty(string name)
+    {
+        var validator = new UpdateDependentValidator();
+        var request = RequestUpdateDependentJsonBuilder.Build();
+        request.Name = name;
+
+        var result = validator.Validate(request);
+        var error = result.Errors.ShouldHaveSingleItem();
+
+        error.ShouldSatisfyAllConditions(e => e.ErrorMessage.ShouldBe(ResourceErrorMessages.EMPTY_NAME));
+        result.IsValid.ShouldBeFalse();
+    }
+}
